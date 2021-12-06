886dba4c283e433303dbcbd63050dc3261f4e9fe
imexme <55784680+imexme@users.noreply.github.com>
2021-12-06 20:47:59 +0100

    Add files via upload

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import HW1 from "./HW1";
+import './Message.css'
+
+function Message(props: any) {
+    return (
+
+        <div className={"message"}>
+
+            <div style={{
+                backgroundImage: `url(${props.avatar})`,
+                height: "50px",
+                width: "50px",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                borderRadius: 25,
+                position:"absolute",
+                marginTop: "5px",
+                marginLeft: "7px"
+            }}>   </div>
+
+            <div className={"messagecont"}>
+                <h3 className={"name"}>{props.name} </h3>
+                 <span className={"singlemessage"}>{props.message} </span>
+                 <span className={"time"}>{props.time} </span>
+                        {/*{messageData}*/}
+            </div>
+        </div>
+
+    )
+}
+
+export default Message
---
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Affair from './Affair'
+import {AffairPriorityType, AffairType, filterAffairs, FilterType} from './HW2'
+import {getPriority} from "os";
+
+
+export type AffairsPropsType = { // need to fix any
+    data: Array<AffairType>
+    setFilter: (filter: FilterType)=> void
+    deleteAffairCallback: (_id:number) => void
+    // filter: FilterType
+}
+
+function Affairs(props: AffairsPropsType) {
+    const mappedAffairs = props.data.map((a: AffairType) => (
+        <Affair // should work
+            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+            affair={a}
+            deleteAffairCallback={props.deleteAffairCallback}
+        />
+    ))
+
+    const setAll = () => {props.setFilter('all')}
+    const setHigh = () => {props.setFilter('high')}
+    const setMiddle = () => {props.setFilter('middle')}
+    const setLow = () => {props.setFilter('low')}
+
+    const set = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        props.setFilter(e.currentTarget.value as FilterType)
+    }
+    return (
+        <div>
+
+            {mappedAffairs}
+
+            <button onClick={setAll}>All</button>
+            <button onClick={setHigh}>High</button>
+            <button onClick={setMiddle}>Middle</button>
+            <button onClick={setLow}>Low</button>
+        </div>
+    )
+}
+
+export default Affairs

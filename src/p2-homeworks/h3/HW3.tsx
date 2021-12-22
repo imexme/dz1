6709796d4ae55ae958facf9c50cb968b9ded8826
imexme <55784680+imexme@users.noreply.github.com>
2021-12-22 14:27:39 +0100

    Add files via upload

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,27 +1,49 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from "uuid";
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
+
+// export type UserType2 = {
+//     _id2: string // need to fix any
+//     name2: string // need to fix any
+// }
+
+
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
+    // const [users2, setUsers2] = useState<Array<UserType2>>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const addUserCallback = (name: string,
+                             // name2: string
+    ) => {
+        const user ={
+            _id: v1(),
+            // _id2: v1(),
+            name,
+            // name2
+        }
+        // need to fix any
+        setUsers([...users, user]) // need to fix
+        // setUsers2([...users2, user]) // need to fix
     }
 
     return (
-        <div>
+        <div>            <hr/>
+            <div> homeworks </div>
             <hr/>
-            homeworks 3
 
             {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer
+                users={users}
+                // users2={users2}
+                addUserCallback={addUserCallback}/>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}

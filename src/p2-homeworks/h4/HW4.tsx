4589e439a2cd966123a4ea5104c69b7ed6af4c65
imexme <55784680+imexme@users.noreply.github.com>
2022-01-13 14:37:25 +0100

    Add files via upload

---
+++ src/p2-homeworks/h4/HW4.tsx
@@ -0,0 +1,94 @@
+import React, {ChangeEvent, useState} from 'react'
+import SuperInputText from './common/c1-SuperInputText/SuperInputText'
+import s from './HW4.module.css'
+import SuperButton from './common/c2-SuperButton/SuperButton'
+import SuperCheckbox from './common/c3-SuperCheckbox/SuperCheckbox'
+
+export function HW4() {
+    const [text, setText] = useState<string>('')
+    // const [text2, setText2] = useState<string>('')
+    const error = text ? '' : 'error'
+
+    const showAlert = () => {
+        if (error) {
+            alert('введите текст...')
+        } else {
+            alert(text) // если нет ошибки показать текст
+        }
+    }
+
+    const [checked, setChecked] = useState<boolean>(false)
+    const testOnChange = (e: ChangeEvent<HTMLInputElement>) => setChecked(e.currentTarget.checked)
+
+    return (
+        <div>
+            <hr/>
+            homeworks 4
+
+            <div className={s.column}>
+                <SuperInputText
+                    value={text}
+                    onChangeText={setText}
+                    onEnter={showAlert}
+                    // spanClassName={s.testSpanError}
+                    error={error}
+
+
+                />
+
+                <SuperInputText
+                    className={s.blue}
+                    //  value={text}
+                    //  onChangeText={setText}
+                    // onEnter={showAlert}
+                    // error={error}
+
+                    // spanClassName={s.testSpanError}
+                />
+
+                {/*----------------------------------------------------*/}
+                <hr/>
+                <SuperButton>
+                    default
+                </SuperButton>
+
+                <SuperButton
+                    red // пропсу с булевым значением не обязательно указывать true
+                    onClick={showAlert}
+                >
+                    delete {/*// название кнопки попадёт в children*/}
+                </SuperButton>
+                <hr/>
+
+                <SuperButton disabled>
+                    disabled
+                </SuperButton>
+
+                {/*----------------------------------------------------*/}
+
+                <SuperCheckbox
+                    checked={checked}
+                    onChangeChecked={setChecked}
+                >
+                    check me hard {/*// этот текст попадёт в children*/}
+                </SuperCheckbox>
+
+                {/*// onChange тоже должен работать*/}
+                <SuperCheckbox
+                    //checked={checked}
+                    onChange={testOnChange}
+                >one more time
+                </SuperCheckbox>
+            </div>
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeSuperInputText/>*/}
+            {/*<AlternativeSuperButton/>*/}
+            {/*<AlternativeSuperCheckbox/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW4
